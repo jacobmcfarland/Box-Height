96e3ac287878d004ea5e85bd3afea41b8c14afbe
Michael Carr <xyeres@me.com>
2022-06-29 15:58:08 -0700

    Allow n number of containers

--- set-height.js
+++ set-height.js
@@ -1,41 +1,41 @@
-function boxResizingStandard()  {   
-    
-    let container = document.querySelector('.standard-line');
+function boxResizingStandard(container) {
+    let lastItem = container.querySelector('.standard-line .last-line');
+    let box = container.querySelector('.standard-line .box');
 
-    let lastItem = document.querySelector('.standard-line .last-line');
-    let box = document.querySelector('.standard-line .box'); 
-
-    var cardButtonHeight = 0; 
-    var cardButton = document.querySelector('.standard-line .card-button');
-    if(cardButton){
-        var cardButtonHeight = cardButton.offsetHeight;
+    var cardButtonHeight = 0;
+    var cardButton = container.querySelector('.standard-line .card-button');
+    if (cardButton) {
+        cardButtonHeight = cardButton.offsetHeight;
     }
 
-    var colButtonHeight = 0; 
-    var colButton = document.querySelector('.standard-line .button-col');
-    if(colButton){
-        var colButtonHeight = colButton.offsetHeight;
+    var colButtonHeight = 0;
+    var colButton = container.querySelector('.standard-line .button-col');
+    if (colButton) {
+        colButtonHeight = colButton.offsetHeight;
     }
-    
+
     const styles = window.getComputedStyle(container);
     const containerHeightNP = container.clientHeight - parseFloat(styles.paddingTop) - parseFloat(styles.paddingBottom);
 
-    //let boxHeight = box.offsetHeight;
-    let lastItemheight = lastItem.offsetHeight;
-    let lastItemHeightHalf = Math.floor(lastItemheight / 2)
-
-    let finalBox = containerHeightNP - 13 - lastItemHeightHalf -  (cardButtonHeight / 2) - (colButtonHeight / 2); 
-
-    box.style.height = finalBox + 'px';
-    console.log(cardButtonHight);
+    if (box && lastItem) {
+        let lastItemheight = lastItem.offsetHeight;
+        let lastItemHeightHalf = Math.floor(lastItemheight / 2)
+        let finalBox = containerHeightNP - 13 - lastItemHeightHalf - (cardButtonHeight / 2) - (colButtonHeight / 2);
+        box.style.height = finalBox + 'px';
     }
+}
 
-    window.onload =  function(){
-            boxResizingStandard();
-        };
+function computeBoxResizing() {
+    // Select all elements with .standard-line class that exist in the DOM
+    const containers = document.querySelectorAll('.standard-line');
+    // Loop over each container and run function on it
+    containers.forEach((container) => {
+        boxResizingStandard(container);
+    })
+}
 
+// Using DOMContentLoaded to detect and run when DOM is fully available
+window.addEventListener('DOMContentLoaded', computeBoxResizing)
 
-    window.onresize =  function(){
-        boxResizingStandard();
-    }
-        
+// Run whenever window is resized
+window.addEventListener('resize', computeBoxResizing)
